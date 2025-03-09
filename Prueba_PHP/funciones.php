1a017d3ef41e5c345630dcd090005a77f8b72db4
fran-quero <fjavier.fj41@gmail.com>
2025-03-09 21:44:07 +0100

    Realizado la funcion del ejercicio numero 4, no realizadas las pruebas de los 3 ultimos ejercicios ya que nose si son necesarias o solo las funciones/clases

--- Prueba_PHP/funciones.php
+++ Prueba_PHP/funciones.php
@@ -60,6 +60,27 @@
     }
 
 
+    //EJERCICIO 4
+    /**
+     * Funcion que devuelve la parte del dia mañana, tarde o noche
+     * 
+     * @return string Devuelve la parte del dia que es
+     */
+    function getPartOfDay():string{
+
+        //Instanciamos un objeto de la clase DateTime
+        $hora_actual = new DateTime();
+
+        //Le cambiamos el formato a 24h y recogemos solo la hora
+        $hora_actual = $hora_actual->format("H");
+
+        //Devolvemos la condicion que se cumpla
+        return match(true){
+            $hora_actual>=6 && $hora_actual<12 => "Mañana",
+            $hora_actual>=12 && $hora_actual<18 => "Tarde",
+            $hora_actual>=18 && $hora_actual<6 => "Noche"
+        };
+    }
 
         
 
