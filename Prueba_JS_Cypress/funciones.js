1f940b1dc294969b4d3b224439142975ebc19ded
fran-quero <fjavier.fj41@gmail.com>
2025-03-10 00:55:36 +0100

    Realizado el ejercicio 4 y su prueba

--- Prueba_JS_Cypress/funciones.js
+++ Prueba_JS_Cypress/funciones.js
@@ -38,10 +38,10 @@ window.onload = function(){
 
     //2. Invertir una cadena de texto
     /**
-     * Invertir una cadena de texto
+     * Invertir una cadena de texto.
      * 
-     * @param {string} entrada Cadena de entrada que se quiere inveritr 
-     * @returns {string} Devuelve la cadena invertida
+     * @param {string} entrada Cadena de entrada que se quiere inveritr. 
+     * @returns {string} Devuelve la cadena invertida.
      */
     function invertirCadena(entrada){
 
@@ -83,6 +83,46 @@ window.onload = function(){
         //Primero se comprueba que esa iteracion no coincida con julio, si coincide acaba el bucle 
         //(tambien comprobamos la i para que no llege a ser mas grande que el array aunque sepamos que no va a pasar)
     }while(mesesAño[i]!="Julio" && i<mesesAño.length);
+
+
+
+
+    //4. Conteo de vocales en una cadena
+
+    let vocales = ["a","e","i","o","u"];
+
+    /**
+     * Devuelve cuantas vocales hay en una cadena.
+     * 
+     * @param {string} entrada Cadena de entrada.
+     * @returns {int} Devuelve el numero de vocales.
+     */
+    function contarVocales(entrada){
+
+        let num_vocales = 0;
+
+        for(i=0;i<entrada.length;i++){
+
+            //Comprobamos cada caracter, si esta sumamos al contador 
+            //(pasamos a lowerCase para asegurar que comprobamos bien segun nuestro array de vocales)
+            if(vocales.includes(entrada[i].toLowerCase())) num_vocales++;
+        }
+
+        return num_vocales;
+    }
+
+    //Prueba de la funcion 4
+    document.getElementById("ej4").onclick = function(){
+
+        let entrada = document.getElementById("entrada_ej4");
+
+        //Muestro en el parrafo el resultado
+        document.getElementById("salida_ej4").value = contarVocales(entrada.value);
+
+        //Limpio el campo de entrada
+        entrada.value="";
+
+    }
 
     
 
