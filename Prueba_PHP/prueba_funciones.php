73ede83e14014343201e13a7f08c92715fe81021
fran-quero <fjavier.fj41@gmail.com>
2025-03-11 15:43:25 +0100

    Terminado fichero de pruebas para las funciones de PHP y la clase Person

--- Prueba_PHP/prueba_funciones.php
+++ Prueba_PHP/prueba_funciones.php
@@ -7,6 +7,7 @@
     <?php
 
         require "funciones.php";
+        require "Person.php";
 
     ?>
     <style>
@@ -71,6 +72,78 @@
 
         <input type="submit" value="Enviar">
     </form>
+
+    <br>
+
+    <h3>Ejercicio 2: Manejo de Arrays</h3>
+
+    <?php
+
+        //Ejemplo de array correcto
+        $array_correcto = [1,2,3,4,5];
+
+        echo "<p>Usaremos el siguiente array de numero:</p>";
+        var_dump($array_correcto);
+
+        $resultado = findLargestNumber($array_correcto);
+
+
+        if(gettype($resultado)=="double") echo "<p>El numero mas grande del array es:$resultado</p>";
+        else echo "<p class='error'>$resultado</p>";
+
+        echo "<br>";
+
+        //Ejemplo de array vacio
+        $array_correcto = [];
+
+        echo "<p>Usaremos el siguiente array de numero:</p>";
+        var_dump($array_correcto);
+
+        $resultado = findLargestNumber($array_correcto);
+
+
+        if(gettype($resultado)=="double") echo "<p>El numero mas grande del array es:$resultado</p>";
+        else echo "<p class='error'>$resultado</p>";
+    ?>
+
+    <br>
+    
+    <h3>Ejercicio 3: Programación Orientada a Objetos</h3>
+
+    <?php
+
+        //Instancia del objeto Person
+        $new_person = new Person("Fran",21,"fjavier.fj41@gmail.com");
+
+        //Uso de alguno de sus metodos
+        echo "<p>Datos del objeto Person:</p>";
+
+        $new_person->showInformation();
+
+        if($new_person->isAdult()){
+            echo "<p>La persona " . $new_person->getName() . " es mayor de edad.</p>";
+        }
+        else{
+            echo "<p>La persona " . $new_person->getName() . " es menor de edad.</p>";
+        }
+
+
+    ?>
+
+    <br>
+    
+    <h3>Ejercicio 4: Parte del Día Según la Hora</h3>
+
+    <?php
+
+        //Llamamos a la funcion y mostramos el resultado
+        $parte_dia = getPartOfDay();
+
+        echo "<p>Actualmente estamos en la $parte_dia</p>";
+
+    ?>
+
+
 
 </body>
 </html>
